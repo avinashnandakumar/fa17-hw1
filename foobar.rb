1b8246a79fbf31305f11448ef2b52faf5afa8b11
Avinash Nandakumar <avinashnandakumar@berkeley.edu>
2017-09-19 20:18:02 -0700

    Finished with HW 1 Final Copy

--- foobar.rb
+++ foobar.rb
@@ -3,7 +3,19 @@ class Foobar
   def self.baz(a)
     # Class method
     # Call with `Foobar.baz`
+     newArray = Array.new
+  	finalArray = Array.new
+  	a.each{ |element| newArray.push(element.to_i) }
+  	newArray = newArray.map{|item| item +=2}
+  	newArray = newArray.uniq
+  	newArray.keep_if{ |a| a<10}
+  	newArray.keep_if{ |a| a%2==0}
+  	sum =0 
+  	newArray.each{|a| sum+=a}
+  	newArray = newArray.map{|item| item.to_s}
+  	return sum
   end
+
 end
 
 
--- hw1.rb
+++ hw1.rb
@@ -5,16 +5,16 @@ end
 
 def sort_array_plus_one(a)
   # Q2 CODE HERE
-
+  a.sort.map{ |element| element+=1}
 end
 
 def combine_name(first_name, last_name)
   # Q3 CODE HERE
-
+  return first_name + " " + last_name 
 end
 
 def blockin_time(a)
-  # DO NOT EDIT THIS CODE BELOW
+  # DO NOT EDIT THIS CODE BELOWcom
   require './foobar'
   Foobar.baz a
 end
